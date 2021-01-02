1b2edbbeb69c0ebd9acf31f1f4d51ac60985d33f
PauliCZ44 <stastnyp@seznam.cz>
2021-01-02 21:24:44 +0100

    google auth

--- client/components/App.js
+++ client/components/App.js
@@ -14,6 +14,7 @@ export default () => (
         content="Calculator for score in board game BANG! based on official scoring table. You can calculate score in real time in table. Bang! is a card game released by DV Giochi in 2002. The game is known worldwide as Bang!, except in France, where it was known as Wanted!. This scoring system is based on this Official tournament scoring system."
       />
       <meta name="robots" content="index, follow" />
+      <meta name="google-site-verification" content="ijUCk4to6Jsdgul6kLRztuF33PJo8i-2oQzbo8YH_hg" />
     </Helmet>
     <NavBar />
     <Router />
