5dcf619b8935fcb70c1c16803b88627cf77645fe
paulicz44 <stastnyp@seznam.cz>
2023-01-01 10:33:32 +0100

    Add dnd WIP + autoAnimate

--- client/components/FrontPage/MainTable.js
+++ client/components/FrontPage/MainTable.js
@@ -1,30 +1,82 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PlayerRow from './PlayerRow'
 import TableHead from './TableHead'
 
+const MainTable = ({ players, rounds, roles, changeRole, changeName, makeDead, makeDuel }) => {
+	const dragItem = useRef()
+	const dragOverItem = useRef()
+	const currentRound = useRef()
+	const roundDrop = useRef()
 
-const MainTable = ({
-  players, rounds, roles, changeRole, changeName, makeDead, makeDuel,
-}) => {
-  const playerRows = players.map((player) => (
-    <PlayerRow player={player} rounds={rounds} key={player.id} roles={roles} changeRole={changeRole} changeName={changeName} makeDead={makeDead} makeDuel={makeDuel} />
-  ))
+	const switchRoles = (id1, id2, round) => {
+		console.log(players)
+		// changeRole()
+	}
 
-  return (
-    <div className="container-xl mt-md-5 max-width-94p">
-      <h3 className="text-center western-font-only">Result table</h3>
-      <div className="table-responsive">
-        <table className="table table-bordered align-middle">
-          <thead className="thead-light">
-            <TableHead rounds={rounds} />
-          </thead>
-          <tbody>
-            {playerRows}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  )
+	const handleDragStart = (e, playerId, round) => {
+		e.dataTransfer.effectAllowed = 'move'
+		currentRound.current = round
+		dragItem.current = playerId
+		console.log('START', playerId, round)
+	}
+
+	const handleDragEnter = (e, playerId, round) => {
+		if (currentRound.current !== round) {
+			console.warn('You can only drop in the same round')
+			e.preventDefault()
+			return
+		}
+		e.preventDefault()
+		e.dataTransfer.effectAllowed = 'move'
+		e.dataTransfer.dropEffect = 'move'
+		roundDrop.current = round
+		dragOverItem.current = playerId
+		console.log('enter', playerId, round)
+	}
+
+	const handleDrop = (e) => {
+		if (currentRound.current !== roundDrop.current) {
+			console.warn('You can only drop in the same round')
+			e.preventDefault()
+			return
+		} else {
+			console.log('DROP', dragItem.current, dragOverItem.current, currentRound.current)
+			console.log(e.target)
+			switchRoles(dragItem.current, dragOverItem.current, currentRound.current)
+		}
+	}
+
+	const playerRows = players.map((player) => (
+		<PlayerRow
+			dragItem={dragItem}
+			dragOverItem={dragOverItem}
+			handleDragStart={handleDragStart}
+			handleDragEnter={handleDragEnter}
+			player={player}
+			rounds={rounds}
+			key={player.id}
+			roles={roles}
+			changeRole={changeRole}
+			changeName={changeName}
+			makeDead={makeDead}
+			makeDuel={makeDuel}
+			handleDrop={handleDrop}
+		/>
+	))
+
+	return (
+		<div className="container-xl mt-md-5 max-width-94p">
+			<h3 className="text-center western-font-only">Result table</h3>
+			<div className="table-responsive">
+				<table className="table table-bordered align-middle">
+					<thead className="thead-light">
+						<TableHead rounds={rounds} />
+					</thead>
+					<tbody>{playerRows}</tbody>
+				</table>
+			</div>
+		</div>
+	)
 }
 
 export default MainTable
--- client/components/FrontPage/PlayerRow.js
+++ client/components/FrontPage/PlayerRow.js
@@ -1,81 +1,143 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react'
-import Form from 'react-bootstrap/Form'
+import React, { useRef } from 'react'
 import numeral from 'numeral'
 import RIP from '../../assets/img/RIP.png'
 import Duel from '../../assets/img/Duel.png'
 
+export const emojis = {
+	Renegade: '🤠',
+	Sheriff: '👮',
+	Deputy: '⭐',
+	Outlaw: '🧔',
+}
+
 const PlayerRow = ({
-  player, rounds, roles, changeRole, changeName, makeDead, makeDuel,
+	player,
+	rounds,
+	changeRole,
+	changeName,
+	makeDead,
+	makeDuel,
+	handleDragStart,
+	handleDragEnter,
+	handleDrop,
 }) => {
-  const handleChangeOption = (e, id, round) => {
-    // console.log('Option changed to:', e.target.value, 'in player ID ==', id, ' in round', round)
-    changeRole(e.target.value, id, round)
-  }
+	const handleChangeOption = (e, id, round) => {
+		// console.log('Option changed to:', e.target.value, 'in player ID ==', id, ' in round', round)
+		changeRole(e.target.value, id, round)
+	}
 
-  const MakeDuelIcon = (i) => (
-    <>
-      <input type="checkbox" id={`IsDuel-P:${player.id}-R:${i}`} className="checkbox-for-dead" checked={player.duels[i]} onChange={(e) => makeDuel(e, player.id, i)} />
-      <label htmlFor={`IsDuel-P:${player.id}-R:${i}`} style={{ marginBottom: '0' }}>
-        <img src={Duel} alt="Is Duel?" className="Duel-img p-1" />
-      </label>
-    </>
-  )
+	const [draggingRoundIndex, setDraggingRoundIndex] = React.useState(-1)
 
-  const emojis = {
-    Renegade: '🤠',
-    Sheriff: '👮',
-    Deputy: '⭐',
-    Outlaw: '🧔',
-  }
-  // tds = cells for each player and each row
-  const tds = []
-  const makeRounds = (num) => {
-    let duel
-    for (let i = 0; i < num; i++) {
-      if (player.roles[i] === 'Renegade') {
-        duel = MakeDuelIcon(i)
-      }
+	const MakeDuelIcon = (i) => (
+		<>
+			<input
+				type="checkbox"
+				id={`IsDuel-P:${player.id}-R:${i}`}
+				className="checkbox-for-dead"
+				checked={player.duels[i]}
+				onChange={(e) => makeDuel(e, player.id, i)}
+			/>
+			<label htmlFor={`IsDuel-P:${player.id}-R:${i}`} style={{ marginBottom: '0' }}>
+				<img src={Duel} alt="Is Duel?" className="Duel-img p-1" />
+			</label>
+		</>
+	)
 
-      tds.push(
-        <React.Fragment key={`Round ${i}`}>
-          <td className="select-role left-border-bold ">
-            <Form.Control value={player.roles[i]} as="select" custom id={`P${player.id}-R${i}`} className="select-role-option" onChange={(e) => handleChangeOption(e, player.id, i)}>
-              {/* Set options to select for each cell */}
-              {roles.map((r, index) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <option key={`P:${player.id}-R:${i}-Rl:${r}${index}`} value={r}>
-                  {emojis[roles[index]] + r}
-                </option>
-              ))}
-            </Form.Control>
-          </td>
-          <td className="zero-padding-y min-width-120 text-center">
+	// tds = cells for each player and each row
+	const tds = []
+	const makeRounds = (num) => {
+		let duel
+		for (let i = 0; i < num; i++) {
+			if (player.roles[i] === 'Renegade') {
+				duel = MakeDuelIcon(i)
+			}
+			const currentRound = i + 1
+			tds.push(
+				<React.Fragment key={`Round ${i}`}>
+					<td className="select-role left-border-bold ">
+						<div
+							className="draggable-role"
+							style={{ opacity: draggingRoundIndex === currentRound ? 0.25 : 1 }}
+							draggable
+							onDragStart={(e) => {
+								handleDragStart(e, player.id, currentRound)
+								setDraggingRoundIndex(currentRound)
+							}}
+							onDragEnd={(e) => {
+								setDraggingRoundIndex(-1)
+								handleDrop(e)
+								e.target.style.backgroundColor = ''
+							}}
+							onDragOver={(e) => {
+								e.preventDefault()
+								e.dataTransfer.dropEffect = 'move'
+								// e.target.style.backgroundColor = '#cacaca'
+							}}
+							onDragLeave={(e) => {
+								e.target.style.backgroundColor = ''
+							}}
+							onDragEnter={(e) => handleDragEnter(e, player.id, currentRound)}
+						>
+							{player.roles[i] + ' ' + emojis[player.roles[i]]}
+						</div>
+						{/* Render arrows down / up to change order */}
+						{/* <span className="arrow">🔼</span>
+						<span className="arrow">🔽</span> */}
+					</td>
+					<td className="zero-padding-y min-width-120 text-center">
+						<input
+							type="checkbox"
+							id={`IsDead-P:${player.id}-R:${i}`}
+							className="checkbox-for-dead"
+							checked={player.deaths[i]}
+							onChange={(e) => makeDead(e, player.id, i)}
+						/>
+						<label htmlFor={`IsDead-P:${player.id}-R:${i}`} style={{ marginBottom: '0' }}>
+							<img src={RIP} alt="Is Duel?" className="RIP-img" />
+						</label>
+						{/* If the player is role, render a duel (that is an JSX containing the checkbox and img of duel card) */}
+						{player.roles[i] === 'Renegade' ? duel : null}
+					</td>
+					<td className="right-border-bold text-align-r">
+						{numeral(player.scores[i]).format('$0,0')}
+					</td>
+				</React.Fragment>
+			)
+		}
+	}
+	makeRounds(rounds)
 
-            <input type="checkbox" id={`IsDead-P:${player.id}-R:${i}`} className="checkbox-for-dead" checked={player.deaths[i]} onChange={(e) => makeDead(e, player.id, i)} />
-            <label htmlFor={`IsDead-P:${player.id}-R:${i}`} style={{ marginBottom: '0' }}>
-              <img src={RIP} alt="Is Duel?" className="RIP-img" />
-            </label>
-            {/* If the player is role, render a duel (that is an JSX containing the checkbox and img of duel card) */}
-            {player.roles[i] === 'Renegade' ? duel : null}
-          </td>
-          <td className="right-border-bold text-align-r">
-            {numeral(player.scores[i]).format('$0,0')}
-          </td>
-        </React.Fragment>,
-      )
-    }
-  }
-  makeRounds(rounds)
-
-  return (
-    <tr>
-      <th className="table-dark th-input-dark" key={player.id}>
-        <input type="text" value={player.name} className="form-control input-dark" onChange={(e) => changeName(e, player.id)} />
-      </th>
-      {tds}
-      <th key={`${player.id}result`} className="text-align-r">{numeral(player.totalScore).format('$0,0')}</th>
-    </tr>
-  )
+	return (
+		<tr>
+			<th className="table-dark th-input-dark" key={player.id}>
+				<input
+					type="text"
+					value={player.name}
+					className="form-control input-dark"
+					onChange={(e) => changeName(e, player.id)}
+				/>
+			</th>
+			{tds}
+			<th key={`${player.id}result`} className="text-align-r">
+				{numeral(player.totalScore).format('$0,0')}
+			</th>
+		</tr>
+	)
 }
 export default PlayerRow
+
+//<Form.Control
+// //value={player.roles[i]}
+// as="select"
+// custom
+// id={`P${player.id}-R${i}`}
+// className="select-role-option"
+// onChange={(e) => handleChangeOption(e, player.id, i)}
+// >
+// {/* Set options to select for each cell */}
+// {roles.map((r, index) => (
+// 	<option key={`P:${player.id}-R:${i}-Rl:${r}${index}`} value={r}>
+// 		{emojis[roles[index]] + r}
+// 	</option>
+// ))}
+// </Form.Control>
