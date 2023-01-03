010422046b37908cc0a92191e4addf33b285ea93
paulicz44 <stastnyp@seznam.cz>
2023-01-03 22:24:40 +0100

    update react.

--- client/index.js
+++ client/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
 import 'Assets/custom.scss'
 import App from 'Components/App'
 import ErrorBoundary from 'Components/ErrorBoundary'
 
-const refresh = () => render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+const refresh = () =>
+	root.render(
+		<BrowserRouter>
+			<ErrorBoundary>
+				<App />
+			</ErrorBoundary>
+		</BrowserRouter>
+	)
 
 refresh()
 
 if (module.hot) {
-  module.hot.accept()
+	module.hot.accept()
 }
